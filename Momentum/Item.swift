3d85891c7280545662962e2995e5f5f88e569787
Oved <ovedramirez56@gmail.com>
2025-11-20 10:56:09 -0600

    Initial Commit

---
+++ Momentum/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Momentum
+//
+//  Created by Oved Ramirez on 11/20/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
