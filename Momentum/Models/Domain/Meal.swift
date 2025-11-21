997a0be2dfe67b6a903f46e39ac80fb583b45241
Oved <ovedramirez56@gmail.com>
2025-11-21 16:36:41 -0600

    I worked on Phase 1 of my App, Created a couple of swift files in order to lay the framework of the project.

---
+++ Momentum/Models/Domain/Meal.swift
@@ -0,0 +1,8 @@
+//
+//  Meal.swift
+//  Momentum
+//
+//  Created by Oved Ramirez on 11/21/25.
+//
+
+import Foundation
---
+++ Momentum/Models/Domain/userProfile.swift
@@ -0,0 +1,8 @@
+//
+//  userProfile.swift
+//  Momentum
+//
+//  Created by Oved Ramirez on 11/21/25.
+//
+
+import Foundation
